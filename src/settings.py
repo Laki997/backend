3faee726acc1d72f7b27622e3c5f9648755707c4
Lakii997 <laki.kalajdzic@gmail.com>
2021-09-13 09:12:38 +0200

    login

--- src/settings.py
+++ src/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'src.users',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -85,6 +86,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 
--- src/urls.py
+++ src/urls.py
@@ -1,11 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from src.users.urls import users_router
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView)
 
 
 router = DefaultRouter()
 router.registry.extend(users_router.registry)
 
 urlpatterns = [
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('login/', TokenObtainPairView.as_view()),
+    path('refresh/token', TokenRefreshView.as_view())
 ]
