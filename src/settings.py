7439d89647855657b69d64cabb43644a8717ae0a
Lakii997 <laki.kalajdzic@gmail.com>
2021-09-14 12:20:26 +0200

    token lifetime

--- src/settings.py
+++ src/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=8),
+}
