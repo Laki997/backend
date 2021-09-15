c7c6e1f363390917d785d6feb1c93e1c48da80f3
Lakii997 <laki.kalajdzic@gmail.com>
2021-09-15 15:18:22 +0200

    settings flake

--- src/settings.py
+++ src/settings.py
@@ -107,12 +107,11 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',),
-        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_AUTHENTICATION_CLASSES':
+        ('rest_framework_simplejwt.authentication.JWTAuthentication', ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
-
-    
 }
 
 
